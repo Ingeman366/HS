6ce15d613427b3bebb288d370f95e18905c9bf79
Gunson91 <113354150+Gunson91@users.noreply.github.com>
2022-10-06 10:10:55 +0200

    Menu update

---
+++ src/ProductList.java
@@ -0,0 +1,10 @@
+public class ProductList {
+
+    private String haircut;
+    private String shampoo;
+    private String hairBrush;
+    private double haircutPrice = 300.00;
+    private double shampooPrice = 50.00;
+    private double hairBrushPrice = 100.00;
+
+}
